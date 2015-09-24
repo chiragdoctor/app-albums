5d1e7ea0783723a572561947859f775319bd70d3
chiragdoctor <chirag.doctor@gmail.com>
2015-09-24 17:22:35 +0100

    Setting up basic node server and express framework

---
+++ index.js
@@ -0,0 +1,10 @@
+var app = require('./app');
+var http = require('http');
+var config = require('./configs/default');
+
+
+var server = http.createServer(app);
+var port = config.port || 3000;
+
+server.listen(port);
+console.log("app-albums has started and listening on port " + config.port);
