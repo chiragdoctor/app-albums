1062d13f782d43523f07a28f60c41e702391ac40
chiragdoctor <chirag.doctor@gmail.com>
2015-09-25 12:36:37 +0100

    remove console logs from init file.

--- oauth2/spotify/init.js
+++ oauth2/spotify/init.js
@@ -4,13 +4,11 @@ var User = require('../../models/user');
 
 module.exports = function (passport, config) {
     passport.serializeUser(function (user, done) {
-        console.log('serializing user: ', user);
         done(null, user._id);
     });
 
     passport.deserializeUser(function (id, done) {
         User.findById(id, function (err, user) {
-            console.log('de-serialized user :', user);
             done(null, user);
         });
     });
