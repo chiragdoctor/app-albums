5ec09a2fa25980a969bf860028545e79f6dfd984
chiragdoctor <chirag.doctor@gmail.com>
2015-09-25 23:23:16 +0100

    refatoring routes and db connection

--- app.js
+++ app.js
@@ -9,16 +9,7 @@ var swig = require('swig');
 var consolidate = require('consolidate');
 var config = require('config');
 
-
-//var homeRoutes = require('./routes/auth/home');
-//var oauthRoutes = require('./routes/auth/oauth');
-//var albumsRoutes = require('./routes/api/albums');
-
-var mongoose = require('mongoose');
-// Connect to DB
-mongoose.connect(config.get('mongo.url'));
-
-
+require('./services/db').connect();
 var app = express();
 
 // view engine setup
@@ -26,8 +17,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-//app.use(bodyParser.json());
-//app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -45,9 +34,6 @@ var initPassport = require('./oauth2/spotify/init');
 initPassport(passport, config);
 
 // Registing routes.
-//app.use('/', homeRoutes);
-//app.use('/oauth', oauthRoutes);
-//app.use('/api', albumsRoutes);
 app.use(require('./routes'));
 
 // catch 404 and forward to error handler
