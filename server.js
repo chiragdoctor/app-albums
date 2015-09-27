cf3e641490dbf9066f290721e6b82165ea7aa20f
chiragdoctor <chirag.doctor@gmail.com>
2015-09-27 23:04:03 +0100

    add port to server.listen

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var config = require('./config/default');
 
 
 var server = http.createServer(app);
+server.listen(app.get('port'));
+
 console.log("app-albums has started and listening on port " + app.get('port'));
